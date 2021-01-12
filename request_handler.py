d2e6a41d7f534aa0cabe35d1e4f6ea8d1dbfd909
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-01-12 12:57:05 -0600

    added POST functionality and imports for customers to init, request, and request handler modules

--- request_handler.py
+++ request_handler.py
@@ -3,7 +3,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal
 from locations import get_all_locations, get_single_location, create_location
 from employees import get_all_employees, get_single_employee, create_employee
-from customers import get_all_customers, get_single_customer
+from customers import get_all_customers, get_single_customer, create_customer
 
 # Here's a class. It inherits from another class.
 
@@ -114,6 +114,9 @@ class HandleRequests(BaseHTTPRequestHandler):
         
         if resource == "employees":
             new_entry = create_employee(post_body)
+        
+        if resource == "customers":
+            new_entry = create_customer(post_body)
 
         # Encode the new animal and send in response
         self.wfile.write(f"{new_entry}".encode())
