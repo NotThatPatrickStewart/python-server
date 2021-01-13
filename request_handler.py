1a1c3dd31e2e8ab2cb3d98ce7832a94ca7726c09
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-01-13 10:12:50 -0600

    added do_PUT function from chapter 6

--- request_handler.py
+++ request_handler.py
@@ -150,6 +150,21 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Encode the new animal and send in response
         self.wfile.write("".encode())
 
+    def do_PUT(self):
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
 # This function is not inside the class. It is the starting
 # point of this application.
