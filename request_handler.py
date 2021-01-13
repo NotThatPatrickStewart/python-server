98639f76560a20c98d5558a13f5ac518b24b18af
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-01-13 09:48:15 -0600

    added imports for delete_employee

--- request_handler.py
+++ request_handler.py
@@ -2,7 +2,7 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal
 from locations import get_all_locations, get_single_location, create_location
-from employees import get_all_employees, get_single_employee, create_employee
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
 
 # Here's a class. It inherits from another class.
