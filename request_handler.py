525830528e8d04aefb809a8e2fd0b8a0748003c5
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-01-13 10:25:46 -0600

    added imports for update_animal

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
